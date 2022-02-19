092f0438419be2badeaba90b8b79a2ad5a653d3f
Attah Emmanuel <attahemma0@gmail.com>
2022-02-19 12:36:32 +0100

    update readme and added activity

---
+++ app/src/main/java/com/itech/wisherbirthdaywishapp/views/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.itech.wisherbirthdaywishapp.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.itech.wisherbirthdaywishapp.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
