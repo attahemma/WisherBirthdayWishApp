3914824ec89618ad0df37ec5490270b8e6fc95d9
Gbohunmi Makinde <gbohunmimakinde@gmail.com>
2022-02-20 16:01:26 +0100

    resolving idea conflict

--- app/src/main/java/com/itech/wisherbirthdaywishapp/views/FriendProfileFragment.kt
+++ app/src/main/java/com/itech/wisherbirthdaywishapp/views/FriendProfileFragment.kt
@@ -5,20 +5,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.itech.wisherbirthdaywishapp.databinding.FragmentFriendProfileBinding
 
-class FriendProfile : Fragment() {
-    private lateinit var _binding: Fragmen
+class FriendProfileFragment : Fragment() {
+    private var _binding: FragmentFriendProfileBinding? = null
+    private val binding get() = _binding!!
 
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    ): View {
+        _binding = FragmentFriendProfileBinding.inflate(layoutInflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
