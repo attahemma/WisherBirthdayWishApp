480eccb717078aa570b8c4202801155209916b2b
Gbohunmi Makinde <gbohunmimakinde@gmail.com>
2022-02-20 15:22:12 +0100

    fragment created

---
+++ app/src/main/java/com/itech/wisherbirthdaywishapp/views/FriendProfile.kt
@@ -0,0 +1,24 @@
+package com.itech.wisherbirthdaywishapp.views
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FriendProfile : Fragment() {
+    private lateinit var _binding: Fragmen
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
