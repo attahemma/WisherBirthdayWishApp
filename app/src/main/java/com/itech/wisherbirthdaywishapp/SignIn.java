6b486ea517e1a39e1b2b8b93fcaa0efd17d12d50
precious3173 <66275437+precious3173@users.noreply.github.com>
2022-02-22 00:38:20 +0100

    SignUp and SignIn

---
+++ app/src/main/java/com/itech/wisherbirthdaywishapp/SignIn.java
@@ -0,0 +1,35 @@
+package com.itech.wisherbirthdaywishapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SignIn extends Fragment {
+
+
+    public SignIn() {
+
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_sign_in, container, false);
+    }
+}
