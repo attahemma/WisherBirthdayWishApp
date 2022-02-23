66b2a1bbe6e5d286c159be9dd5511d9bd4605945
gentlekboy <gentlekboy@gmail.com>
2022-02-23 19:15:33 +0100

    set up adapter for recycler view

--- app/src/main/java/com/itech/wisherbirthdaywishapp/views/ui/upcomingscreen/UpcomingScreenFragment.kt
+++ app/src/main/java/com/itech/wisherbirthdaywishapp/views/ui/upcomingscreen/UpcomingScreenFragment.kt
@@ -1,16 +1,19 @@
 package com.itech.wisherbirthdaywishapp.views.ui.upcomingscreen
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.itech.wisherbirthdaywishapp.R
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.itech.wisherbirthdaywishapp.databinding.FragmentUpcomingScreenBinding
+import com.itech.wisherbirthdaywishapp.model.UpcomingScreenModel
+import com.itech.wisherbirthdaywishapp.views.utils.UpcomingScreenClickInterface
 
-class UpcomingScreenFragment : Fragment() {
+class UpcomingScreenFragment : Fragment(), UpcomingScreenClickInterface {
     private var _binding: FragmentUpcomingScreenBinding? = null
     private val binding get() = _binding!!
+    private lateinit var adapter: UpcomingScreenAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,10 +25,35 @@ class UpcomingScreenFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val listOfFriends = mutableListOf(
+            UpcomingScreenModel("Konohamaru Sarutobi", "02 Feb"),
+            UpcomingScreenModel("Sakura Haruno", "22 Jun"),
+            UpcomingScreenModel("Hyuga Hinata", "09 Dec"),
+            UpcomingScreenModel("Hatake Kakashi", "15 Jan"),
+            UpcomingScreenModel("Toneri Ōtsutsuki", "05 Mar"),
+            UpcomingScreenModel("Uchiha Madara", "20 May"),
+            UpcomingScreenModel("Uchiha Sasuke", "19 Aug"),
+            UpcomingScreenModel("Uzumaki Naruto", "15 Apr"),
+            UpcomingScreenModel("Uchiha Sarada", "23 Nov"),
+            UpcomingScreenModel("Rock Lee", "30 Sep"),
+            UpcomingScreenModel("Akimichi Choji", "11 Jul"),
+            UpcomingScreenModel("Inuzuka Kiba", "28 Oct"),
+        )
+
+        adapter = UpcomingScreenAdapter(listOfFriends, this)
+        binding.upcomingScreenRv.apply {
+            adapter = adapter
+            setHasFixedSize(true)
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun clickFriendCard(position: Int) {
+        Toast.makeText(requireContext(), "You clicked item $position", Toast.LENGTH_SHORT).show()
     }
 }
